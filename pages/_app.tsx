eddb5fabfede976bd661ae062c6938b06fa67a11
elorapeter <florenceonyi09@gmail.com>
2025-11-19 10:01:57 +0000

    Nov 19, 2025, 11:01 AM

---
+++ pages/_app.tsx
@@ -0,0 +1,16 @@
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import Head from 'next/head'
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" href="/icons/icon-192x192.png" />
+        <meta name="theme-color" content="#4f46e5" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
+}
